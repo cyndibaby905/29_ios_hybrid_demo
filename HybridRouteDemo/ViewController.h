6fd49a7af12a551900aac024340ab8d97f66be37
Hang Chen <hangisnice@gmail.com>
2019-07-26 20:10:27 +0800

    Initial Commit

---
+++ HybridRouteDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HybridRouteDemo
+//
+//  Created by Hang Chen on 2019/7/26.
+//  Copyright © 2019 Hang Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HybridRouteDemoTests/HybridRouteDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  HybridRouteDemoTests.m
+//  HybridRouteDemoTests
+//
+//  Created by Hang Chen on 2019/7/26.
+//  Copyright © 2019 Hang Chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HybridRouteDemoTests : XCTestCase
+
+@end
+
+@implementation HybridRouteDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
